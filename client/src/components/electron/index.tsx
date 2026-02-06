02b8bde1a6135ae26308be9a5dd4cd9be9928f50
Mqhele-dot <89026892+Mqhele-dot@users.noreply.github.com>
2026-02-06 22:16:26 +0200

    Delete client directory

--- client/src/components/electron/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-export { TitleBar } from './title-bar';
-export { UpdateNotification } from './update-notification';
-export { OfflineModeIndicator } from './offline-mode-indicator';
-export { LocalDatabaseInfo } from './local-database-info';
